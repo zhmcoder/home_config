45c7cf1e254698b61044dca50a3ed7d4f426ca09
zhaomeng <zhmzhaomeng@hotmail.com>
2021-11-07 08:14:56 +0800

    接口测试

---
+++ src/Services/HomeConfigService.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Andruby\HomeConfig\Services;
+
+
+use Andruby\HomeConfig\Models\HomeConfig;
+use App\Api\Libs\WXBizDataCrypt;
+use Cache;
+
+class HomeConfigService
+{
+    public static function home_data($app_id, $os_type, $date_time)
+    {
+        $homeConfigList = HomeConfig::query()->where('shelf_on', 1)
+            ->where('show_app', 'like', '%"' . $app_id . '"%')
+            ->where(function ($query) use ($date_time) {
+                $query->where('publish_up', '<=', $date_time)
+                    ->orWhere(function ($query) {
+                        $query->where('publish_up', null);
+                    });
+            })
+            ->where(function ($query) use ($date_time) {
+                $query->where('publish_down', '>=', $date_time)
+                    ->orWhere(function ($query) {
+                        $query->where('publish_down', null);
+                    });
+            })
+            ->orderBy('sort', 'asc')->orderBy('id', 'desc')->get()->toArray();
+
+        return $homeConfigList;
+    }
+}
